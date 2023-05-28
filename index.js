9882e5cd453d16ad506e518e8d617acae416bb3c
Mushroom <123withnumber@gmail.com>
2023-05-28 11:31:53 +0530

    Bug fix: Newly added users not being loaded

--- index.js
+++ index.js
@@ -22,75 +22,94 @@ function logMessage (message) {
 
 function checkMessage (message) {
   if (message.author.bot) return
+  const users = JSON.parse(fs.readFileSync('users.json'))
 
   if (message.content.startsWith(prefix)) {
     const args = message.content.slice(prefix.length).split(/ +/)
     const command = args.shift().toLowerCase()
     if (command === 'shadowbans') {
-      if (message.member.permissions.has('MANAGE_ROLES')) {
-        const users = config.users.map((user) => `<@!${user}>\n`)
-        const reply = `**Wanted List**:
-        ${users}
-        `
-        message.channel.send(reply, { allowedMentions: { users: [] } })
-      } else {
-        message.channel.send('Nice try, bucko! You need to be a mod to use this command')
-      }
+      showShadowBans(message, users)
     }
 
     if (command === 'addshadowban') {
-      if (!message.member.permissions.has('MANAGE_ROLES')) {
-        message.channel.send('Nice try, bucko! You need to be a mod to use this command')
-        return
-      }
-
-      if (args.length === 0) {
-        message.channel.send('I need to see ID, Pal')
-        return
-      }
-
-      if (config.users.includes(args[0])) {
-        message.channel.send('A man cannot be wanted twice at the same time, bub')
-        return
-      }
-
-      const newConfig = { ...config }
-      newConfig.users.push(
-        args[0]
-      )
-      fs.writeFileSync('config.json', JSON.stringify(newConfig, null, '\t'))
-      message.channel.send('I\'ll keep an eye out for \'em')
+      addShadowBan(message, args, users)
     }
 
     if (command === 'removeshadowban') {
-      if (!message.member.permissions.has('MANAGE_ROLES')) {
-        message.channel.send('Nice try, bucko! You need to be a mod to use this command')
-        return
-      }
-
-      if (args.length === 0) {
-        message.channel.send('I need to see ID, Pal')
-        return
-      }
-
-      if (!config.users.includes(args[0])) {
-        message.channel.send('Who even is this? Not on my list')
-        return
-      }
-
-      const newConfig = { ...config }
-      const index = newConfig.users.indexOf(args[0])
-      if (index > -1) {
-        newConfig.users.splice(index, 1)
-      }
-      fs.writeFileSync('config.json', JSON.stringify(newConfig, null, '\t'))
-      message.channel.send('They\'re free now')
+      removeShadowBan(message, args, users)
     }
   } else {
     // check for message sent by author
-    if (config.users.includes(message.author.id)) {
+    if (users.includes(message.author.id)) {
       message.delete() // delete message
       logMessage(message) // log message
     }
   }
+}
+
+function showShadowBans (message, users) {
+  if (message.member.permissions.has('MANAGE_ROLES')) {
+    let reply
+    if (users.length > 0) {
+      const shadowBannedUsers = users.map((user) => `<@!${user}>\n`)
+      reply = `**Wanted List**:
+      ${shadowBannedUsers}
+      `
+    } else {
+      reply = 'You have a crime free town here, partner'
+    }
+
+    message.channel.send(reply, { allowedMentions: { users: [] } })
+  } else {
+    message.channel.send('Nice try, bucko! You need to be a mod to use this command')
+  }
+}
+
+function removeShadowBan (message, args, users) {
+  if (!message.member.permissions.has('MANAGE_ROLES')) {
+    message.channel.send('Nice try, bucko! You need to be a mod to use this command')
+    return
+  }
+
+  if (args.length === 0) {
+    message.channel.send('I need to see ID, Pal')
+    return
+  }
+
+  if (!users.includes(args[0])) {
+    message.channel.send('Who even is this? Not on my list')
+    return
+  }
+
+  const newUsers = [...users]
+  const index = newUsers.indexOf(args[0])
+  if (index > -1) {
+    newUsers.splice(index, 1)
+  }
+  fs.writeFileSync('users.json', JSON.stringify(newUsers, null, '\t'))
+  message.channel.send('They\'re free now')
+}
+
+function addShadowBan (message, args, users) {
+  if (!message.member.permissions.has('MANAGE_ROLES')) {
+    message.channel.send('Nice try, bucko! You need to be a mod to use this command')
+    return
+  }
+
+  if (args.length === 0) {
+    message.channel.send('I need to see ID, Pal')
+    return
+  }
+
+  if (users.includes(args[0])) {
+    message.channel.send('A man cannot be wanted twice at the same time, bub')
+    return
+  }
+
+  const newUsers = [...users]
+  newUsers.push(
+    args[0]
+  )
+  fs.writeFileSync('users.json', JSON.stringify(newUsers, null, '\t'))
+  message.channel.send('I\'ll keep an eye out for \'em')
 }
